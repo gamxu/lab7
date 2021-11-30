bcb980591b478ea865fe7861067cd04767643ccf
gamxu <wuttipat.did@gmail.com>
2021-11-30 15:35:11 +0700

    lab7_2 complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,13 +1,37 @@
 #include<iostream>
-
 using namespace std;
-
-int main(){
-	
+int main()
+{
 	int i = 0, P[5000], k, result;
-	while(i < 5000){
+	while(i < 5000)
+	{
 		P[i] = 2*i+1;
 		i++;
+	}
+
+	cout << "Please input k: ";
+	cin >> k;
+
+	if(k>0)
+	{
+		int i=0, result = 0;
+			while(i<5000)
+			{
+				if(P[i]%k==0)
+				{
+					result -= P[i];
+					i++;
+				}else
+				{
+					result += P[i];
+					i++;
+				}
+			}
+	    cout << "Result = " << result;
+
+	}else
+	{
+		cout << "Invalid input!!!";
 	}
 	
 	return 0;
