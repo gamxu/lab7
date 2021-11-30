a3a5f7c6d1d13bd461328ea302e80d7f5767c946
gamxu <wuttipat.did@gmail.com>
2021-11-30 16:09:13 +0700

    lab7_3 complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,45 @@
 
 using namespace std;
 
+int adiff(int a, int b){
+  
+  int c;
+  while(a >= 360)
+  {
+    a -= 360;
+  }
+
+  while(b >= 360)
+  {
+    b -= 360;
+  }
+
+  if(a>b)
+  {
+    c = a - b;
+
+  }else
+  {
+    c = b - a;
+  }
+
+  if(c >= 180)
+  {
+    c = 360 - c;
+  }
+  
+  return c;
+}
+
+
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << "\n";
+  cout << adiff(210,45) << "\n";
+  cout << adiff(0,360) << "\n";
+  cout << adiff(10,350) << "\n";
+  cout << adiff(95,260) << "\n";
+  cout << adiff(90,-90) << "\n";
+  cout << adiff(1000,280) << "\n";
+  cout << adiff(60,244) << "\n";
 }
